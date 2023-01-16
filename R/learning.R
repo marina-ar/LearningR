b278d7406c1cafa36265419fceb1205c6045b264
Marina Armeni <armeni@net.chalmers.se>
2023-01-16 16:00:05 +0100

    addition of .R file

---
+++ R/learning.R
@@ -0,0 +1,18 @@
+10
+
+# R basics ----------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos <- 10
+
+colnames(airquality)
+str(airquality)
+summary(airquality)
+
+2 + 2
+
+2 + 2
+
+
+# Packages ----------------------------------------------------------------
+library(tidyverse)
