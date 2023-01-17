1ec48f5864d55277aa555e18052fa0e61eebb512
Marina Armeni <armeni@net.chalmers.se>
2023-01-17 11:55:44 +0100

    practe pipe

--- R/learning.R
+++ R/learning.R
@@ -76,3 +76,28 @@ nhanes_small %>%
 nhanes_small %>%
   select(phys_active) %>%
   rename(physically_active = phys_active)
+
+# practice ----------------------------------------------------------------
+
+nhanes_small %>%
+  select(
+    bp_sys_ave,
+    education
+  )
+
+nhanes_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave
+  )
+
+nhanes_small %>%
+  select(
+    bmi,
+    contains("age")
+  )
+
+
+nhanes_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
