2bbcfd7ccca9235de6bc666ff03e371a95231264
Marina Armeni <armeni@net.chalmers.se>
2023-01-17 11:38:39 +0100

    showing off the pipe

--- R/learning.R
+++ R/learning.R
@@ -50,3 +50,29 @@ nhanes_small <- select(
   Education
 )
 nhanes_small
+
+
+# Fixing variable names ---------------------------------------------------
+
+# change teh camel case to snake format
+nhanes_small <- rename_with(
+  nhanes_small,
+  snakecase::to_snake_case
+)
+nhanes_small
+
+nhanes_small <- rename(
+  nhanes_small,
+  sex = gender
+)
+nhanes_small
+
+
+# piping ------------------------------------------------------------------
+
+colnames(nhanes_small)
+nhanes_small %>%
+  colnames()
+nhanes_small %>%
+  select(phys_active) %>%
+  rename(physically_active = phys_active)
