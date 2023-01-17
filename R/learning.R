217cc3959e6330204233434e9d8b0117bf3b7644
Marina Armeni <armeni@net.chalmers.se>
2023-01-17 11:15:59 +0100

    Made smaller nhanes dataset

--- R/learning.R
+++ R/learning.R
@@ -17,6 +17,36 @@ summary(airquality)
 
 # Packages ----------------------------------------------------------------
 library(tidyverse)
+library(NHANES)
+# This will be used for testing out Git
 
-#This will be used for testing out Git
 
+
+# looking at data ---------------------------------------------------------
+
+glimpse(NHANES)
+
+# data frame is not changed, we juset select a part of it
+select(NHANES, Age, Weight, BMI)
+
+# drop a variable
+select(NHANES, -HeadCirc)
+
+# select all variables that start with BP, similar functions: ends_with adn contains
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+select(NHANES, contains("Age"))
+
+nhanes_small <- select(
+  NHANES,
+  Age,
+  Gender,
+  BMI,
+  Diabetes,
+  PhysActive,
+  BPSysAve,
+  BPDiaAve,
+  Education
+)
+nhanes_small
